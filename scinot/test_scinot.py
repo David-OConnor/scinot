5ad6e541f7366a503676b46768645edda2a4caaa
ranibaron <ranibaron@gmail.com>
2017-12-23 21:11:12 +0200

    update tests from scinot.disp to scinot.sciprint

--- scinot/test_scinot.py
+++ scinot/test_scinot.py
@@ -24,45 +24,45 @@ def test_start_end_sequence(capsys):
     out, err = capsys.readouterr()
     assert out == "12345678\n"
 
-def test_disp(capsys):
+def test_sciprint(capsys):
     # disply scinot with a standard number
-    scinot.disp(1234567890)
+    scinot.sciprint(1234567890)
     sys.stderr.write("error\n")
     out, err = capsys.readouterr()
     assert out == "1.235 × 10⁹\n"
 
     # disply scinot with a decimal fraction
-    scinot.disp(0.000000001)
+    scinot.sciprint(0.000000001)
     sys.stderr.write("error\n")
     out, err = capsys.readouterr()
     assert out == "1 × 10⁻⁹\n"
 
     # disply scinot with a standard number and controlled amount of significant figures
-    scinot.disp(1234567890, 7)
+    scinot.sciprint(1234567890, 7)
     sys.stderr.write("error\n")
     out, err = capsys.readouterr()
     assert out == "1.234568 × 10⁹\n"
 
     # disply scinot with standard number and 0 as digits count
-    scinot.disp(1234567890, 0)
+    scinot.sciprint(1234567890, 0)
     sys.stderr.write("error\n")
     out, err = capsys.readouterr()
     assert out == "0 × 10⁹\n"  # TODO I would expect and error message for amount of significant figures <= 0
 
     # disply scinot with a negative number
-    scinot.disp(-1234567890)
+    scinot.sciprint(-1234567890)
     sys.stderr.write("error\n")
     out, err = capsys.readouterr()
     assert out == "-1.235 × 10⁹\n"
 
     # disply scinot with a negative number and controlled amount of significant figures
-    scinot.disp(-1234567890,2)
+    scinot.sciprint(-1234567890,2)
     sys.stderr.write("error\n")
     out, err = capsys.readouterr()
     assert out == "-1.2 × 10⁹\n"
 
     # disply scinot with a negative number and controlled amount of significant figures
-    scinot.disp(-.00000409348, 2)
+    scinot.sciprint(-.00000409348, 2)
     sys.stderr.write("error\n")
     out, err = capsys.readouterr()
     assert out == "-4.1 × 10⁻⁶\n"
